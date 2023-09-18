1562dfecf6332034264c23b7c05e2223b2bbc140
Kim <kimdonnarumma@gmail.com>
2023-09-18 19:09:58 -0400

    hii

--- src/EventOrganizer.java
+++ src/EventOrganizer.java
@@ -2,5 +2,19 @@
  * @Kimberly Donnarumma
  * @
  */
+import java.util.Scanner;
 public class EventOrganizer {
+    public void run(){
+        System.out.println("Event Organizer Running...");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("asfdasdfasd");
+        String newInput = "";
+        while(!newInput.equals("q")){
+            newInput = scanner.nextLine();
+            System.out.println("input: " + newInput);
+        }
+
+        scanner.close();
+    }
 }
